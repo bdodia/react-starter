809d6ca34bf4799ab2131739db8ee12df919a5dd
bdodia <bdodia@users.noreply.github.com>
2018-12-28 20:37:20 +0000

    component refactor

--- src/app.js
+++ src/app.js
@@ -1,44 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ColourSwatch } from "./colourSwatch";
-import "./colourSwitch";
+
+import { ColourSwitch } from "./ColourSwitch";
 
 let colourRange = ["Blue", "Red", "Green", "Pink", "Purple", "Cream", "Brown"];
-
-class ColourSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getRandomIndex = this.getRandomIndex.bind(this);
-    this.getColour = this.getColour.bind(this);
-    this.state = {
-      colourIndex: 0
-    };
-  }
-
-  getColour(index) {
-    return colourRange[index];
-  }
-
-  getRandomIndex() {
-    this.setState(() => {
-      return {
-        colourIndex: Math.floor(Math.random() * colourRange.length)
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <ColourSwatch />
-        <button onClick={this.getRandomIndex}>Switch Colour </button>
-        <p>
-          Colour selected: {this.state.colourIndex} :{" "}
-          {this.getColour(this.state.colourIndex)}
-        </p>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<ColourSwitch />, document.getElementById("app"));
--- src/colourSwitch.js
+++ src/colourSwitch.js
@@ -1,3 +1,41 @@
-console.log("from colour switch");
+import React from "react";
+import { ColourSwatch } from "./ColourSwatch";
 
-export {};
+let colourRange = ["Blue", "Red", "Green", "Pink", "Purple", "Cream", "Brown"];
+
+class ColourSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getRandomIndex = this.getRandomIndex.bind(this);
+    this.getColour = this.getColour.bind(this);
+    this.state = {
+      colourIndex: 0
+    };
+  }
+
+  getColour(index) {
+    return colourRange[index];
+  }
+
+  getRandomIndex() {
+    this.setState(() => {
+      return {
+        colourIndex: Math.floor(Math.random() * colourRange.length)
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ColourSwatch />
+        <button onClick={this.getRandomIndex}>Switch Colour </button>
+        <p>
+          Colour selected: {this.state.colourIndex} :{" "}
+          {this.getColour(this.state.colourIndex)}
+        </p>
+      </div>
+    );
+  }
+}
+export { ColourSwitch };
