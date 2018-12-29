7f3c96ea50a0d5a69a42855bc00185d54bf9f133
bdodia <bdodia@users.noreply.github.com>
2018-12-29 16:00:41 +0000

    colour bar initial logic

--- src/colourSwatch.js
+++ src/colourSwatch.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+let colourRange = ["Blue", "Red", "Green", "Pink", "Purple", "Cream", "Brown"];
+
+const colourBar = () => {
+  let colourBarHTML = "";
+  colourRange.forEach(c => {
+    colourBarHTML = colourBarHTML + " " + c;
+  });
+  return colourBarHTML;
+};
+
 const ColourSwatch = () => {
   return (
     <div>
-      <p>React project using webpack and babel set-up!!!</p>
+      <h1>React project using webpack and babel set-up</h1>
       <p>Colour swatch goes here</p>
+      {colourBar()}
     </div>
   );
 };
