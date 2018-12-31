5b891e14dce7d640415773468b05e5a8546ed79c
bdodia <bdodia@users.noreply.github.com>
2018-12-31 22:16:26 +0000

    addition of transform class properties plugin

--- src/app.js
+++ src/app.js
@@ -5,4 +5,30 @@ import { ColourSwitch } from "./ColourSwitch";
 
 let colourRange = ["Blue", "Red", "Green", "Pink", "Purple", "Cream", "Brown"];
 
+// class numberCheck {
+//   constructor() {
+//     this.numberValue = 5;
+//     this.someAdder = this.someAdder.bind(this);
+//   }
+
+//   someAdder() {
+//     return `10 ${this.numberValue}`;
+//   }
+// }
+
+// class newSyntax {
+//   numberValue = 2334;
+//   someAdder = () => {
+//     return `10 ${this.numberValue}`;
+//   };
+// }
+
+// const newVersion = new newSyntax();
+// console.log(newVersion.someAdder());
+
+// const myNumber = new numberCheck();
+// const getNumber = myNumber.someAdder;
+// console.log(myNumber.numberValue);
+// console.log(getNumber());
+
 ReactDOM.render(<ColourSwitch />, document.getElementById("app"));
