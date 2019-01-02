9b468f6776cb28add7608a9bc3e86fc8cac6cabd
bdodia <bdodia@users.noreply.github.com>
2019-01-02 10:47:42 +0000

    clean up

--- src/app.js
+++ src/app.js
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom";
 import { ColourSwitch } from "./ColourSwitch";
 import "./styles/styles.scss";
 
-let colourRange = ["Blue", "Red", "Green", "Pink", "Purple", "Cream", "Brown"];
-
 ReactDOM.render(<ColourSwitch />, document.getElementById("app"));
